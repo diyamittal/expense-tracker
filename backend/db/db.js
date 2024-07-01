11bd406abe1e5aec3e218e9db73c3eb07469e1a1
diyamittal <mittal22.diya@gmail.com>
2024-07-01 15:55:11 +0530

    first commit

---
+++ backend/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const db = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Db Connected')
+    } catch (error) {
+        console.log('DB Connection Error', error.message);
+    }
+}
+
+module.exports = {db}
